8c907678daf87460f26fc97f91383111cd47d300
jw910731 <jw910731@gmail.com>
2023-10-12 11:52:26 +0800

    Toolchain setup

--- javascript/main.js
+++ javascript/main.js
@@ -2,7 +2,7 @@
 let wasm_module;
 
 // replace this with the name of your module
-const MODULE_NAME = "screeps-starter-rust";
+const MODULE_NAME = "screeps";
 
 function console_error(...args) {
     console.log(...args);
